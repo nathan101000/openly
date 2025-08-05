fe219d0e7a2adca7397b131083ac6305e65ba3ee
Nathan Mittelman <nachmimittelman@gmail.com>
2025-08-05 11:01:07 -0400

    fix(android): disable minify and shrinkResources in release build
    This disables R8 minification and resource shrinking for the release build,
    to test and resolve a crash occurring only in GitHub Actions-built APKs.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -70,6 +70,8 @@ android {
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = false
+            isShrinkResources = false
         }
     }
 }
